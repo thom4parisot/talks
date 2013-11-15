6428d9f09d104f9cc2af785916196347d27be3ea
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-11-15 10:23:23 +0000

    Automatic deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,25 @@
 
 module.exports = function(grunt){
   grunt.initConfig({
+   'now': (new Date).getFullYear(),
+   'gh-pages': {
+      gh: {
+        src: [
+          '{2012..<%= now %>}/**/*',
+          '*.jpg',
+          'src/**/*',
+          'bower_components/reveal.js/css/**/*.css',
+          'bower_components/reveal.js/lib/css/zenburn.css',
+          'bower_components/reveal.js/lib/js/head.min.js',
+          'bower_components/reveal.js/js/**/*.js',
+          'bower_components/reveal.js/plugin/{highlight,markdown}/*.js'
+        ],
+        options: {
+          base: './'
+        }
+      }
+    },
+
     connect: {
       dev: {
         options: {
@@ -14,6 +33,8 @@ module.exports = function(grunt){
   });
 
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   grunt.registerTask('default', ['connect:dev:keepalive']);
+  grunt.registerTask('deploy', ['gh-pages']);
 };
