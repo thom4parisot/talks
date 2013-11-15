2807f1a886538ba884aece0496370ac84e4c99bb
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-11-15 11:18:40 +0000

    Copying content to avoid bower_component to be ignored

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,30 @@
 module.exports = function(grunt){
   grunt.initConfig({
    'now': (new Date).getFullYear(),
+
+    copy: {
+      core: {
+        src: [
+          '{2010..<%= now %>}/**/*',
+          'src/**/*',
+          'bower_components/reveal.js/{css,js,lib}/**/*.{css,js}',
+          'bower_components/reveal.js/plugin/{markdown,highlight}/**/*.{css,js}'
+        ],
+        dest: 'dist/'
+      },
+      bower_trick: {
+        expand: true,
+        flatten: true,
+        src: 'src/.gitignore',
+        dest: 'dist/'
+      }
+    },
+
    'gh-pages': {
       gh: {
-        src: [
-          '{2012..<%= now %>}/**/*',
-          '*.jpg',
-          'src/**/*',
-          'bower_components/reveal.js/css/**/*.css',
-          'bower_components/reveal.js/lib/css/zenburn.css',
-          'bower_components/reveal.js/lib/js/head.min.js',
-          'bower_components/reveal.js/js/**/*.js',
-          'bower_components/reveal.js/plugin/{highlight,markdown}/*.js'
-        ],
+        src: '**/*',
         options: {
-          base: './'
+          base: 'dist/'
         }
       }
     },
@@ -26,15 +36,17 @@ module.exports = function(grunt){
         options: {
           port: process.env.PORT || 3000,
           open: true,
-          debug: true
+          debug: true,
+          base: 'dist/'
         }
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-gh-pages');
 
-  grunt.registerTask('default', ['connect:dev:keepalive']);
-  grunt.registerTask('deploy', ['gh-pages']);
+  grunt.registerTask('default', ['copy', 'connect:dev:keepalive']);
+  grunt.registerTask('deploy', ['copy', 'gh-pages']);
 };
