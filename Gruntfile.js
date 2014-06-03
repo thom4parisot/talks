87eb73abf6ccdd7916223a5761018fcf802bb352
Thomas Parisot <thomas.parisot@bbc.co.uk>
2014-06-03 15:25:33 +0100

    Please ignore the outlines of the builds.

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function(grunt){
      presentations: {
        src: [
          '<%= src %>/*/*.md',
-         '!outline.md'
+         '!<%= src %>/*/outline.md'
        ],
        dest: 'dist/',
        options: {
